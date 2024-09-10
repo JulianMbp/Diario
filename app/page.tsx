fd615fc7d708334d42a8f04d42b9e74315870a34
julian <julilito09@gmail.com>
2024-09-10 18:49:09 -0500

    adicion mascotas

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,47 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <header className="bg-white">
-      <ul className="flex">
-          <li className="flex-1 mr-2">
-            <a className="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white" href="#">Notas</a>
-          </li>
-          <li className="flex-1 mr-2">
-            <a className="text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="#">Nota2</a>
-          </li>
-  </ul>
+    <main className=" bg-[#F5EEE3] h-full w-full divide-slate-300 divide-y">
+      <h1 className=" text-5xl font-bold text-center pb-5 pt-5">
+        Nuestra Familia
+      </h1>
+      <header className=" ">
+        <nav className="px-4 py-4 sm:flex sm:items-center sm:justify-between">
+          <div className="flex flex-col items-star mt-3 gap-2 sm:flex-row sm:m-0">
+            <a className="text-gray-600 hover:bg-[#FDC17A] w-full text-left p-2 rounded hover:text-gray-900 order-1" href="#luna">Luna</a>
+            <a className="text-gray-600 hover:bg-[#FDC17A] w-full text-left p-2 rounded hover:text-gray-900 order-4" href="#nacho">Nacho</a>
+            <a className="text-gray-600 hover:bg-[#FDC17A] w-full text-left p-2 rounded hover:text-gray-900 order-2" href="#samy">Samy</a>
+            <a className="text-gray-600 hover:bg-[#FDC17A] w-full text-left p-2 rounded hover:text-gray-900 order-7" href="#bruno">Bruno</a>
+            <a className="text-gray-600 hover:bg-[#FDC17A] w-full text-left p-2 rounded hover:text-gray-900 order-3" href="#maya">Maya</a>
+            <a className="text-gray-600 hover:bg-[#FDC17A] w-full text-left p-2 rounded hover:text-gray-900 order-6" href="#popocha">Popocha</a>
+            <a className="text-gray-600 hover:bg-[#FDC17A] w-full text-left p-2 rounded hover:text-gray-900 order-6" href="#browni">Brownie</a>
+          </div>
+        </nav>
       </header>
-      <h1>hola</h1>
-      <footer className="bg-blue-300">footer</footer>
+      <section className="divide-y divide-slate-300 flex flex-col ">
+          <section id="luna" className="text-center">
+            Luna
+          </section>
+          <section id="nacho" className="text-center">
+            Nacho
+          </section>
+          <section id="samy" className="text-center">
+            Samy
+          </section>
+          <section id="bruno" className="text-center">
+            Bruno
+          </section>
+          <section id="maya" className="text-center">
+            Maya
+          </section>
+          <section id="popocha" className="text-center"> 
+            Popocha
+          </section>
+          <section id="browni" className="text-center">
+            Brownie
+          </section>
+      </section>
+      <footer className="text-center p-10">footer</footer>
     </main>
   );
 }
